ac4d9de92fcb768e63fd6d0e7b442c8351f7dee6
Elif Özcan <ozcann159@gmail.com>
2024-07-27 20:51:50 +0300

    Add functionality for adding and updating todos in TodoFormPage

--- lib/pages/todo_form_page.dart
+++ lib/pages/todo_form_page.dart
@@ -1,91 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../controllers/todo_controller.dart';
 
-class TodoFormPage extends StatefulWidget {
-  @override
-  _TodoFormPageState createState() => _TodoFormPageState();
-}
+class TodoFormPage extends StatelessWidget {
+  final String? todoId;
+  final bool isUpdate;
 
-class _TodoFormPageState extends State<TodoFormPage> {
-  final _titleController = TextEditingController();
-  bool _isCompleted = false;
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  void initState() {
-    super.initState();
-    final todoId = Get.arguments;
-    if (todoId != null) {
-      _loadTodoData(todoId);
-    }
-  }
-
-  Future<void> _loadTodoData(String todoId) async {
-    final doc = await FirebaseFirestore.instance.collection('todos').doc(todoId).get();
-    final data = doc.data();
-    setState(() {
-      _titleController.text = data?['title'] ?? '';
-      _isCompleted = data?['completed'] ?? false;
-    });
-  }
-
-  void _saveTodo() async {
-    if (_formKey.currentState?.validate() ?? false) {
-      final todoData = {
-        'title': _titleController.text,
-        'completed': _isCompleted,
-        'createdAt': Timestamp.now(),
-      };
-
-      final todoId = Get.arguments;
-      if (todoId != null) {
-        // Güncelle
-        await FirebaseFirestore.instance.collection('todos').doc(todoId).update(todoData);
-      } else {
-        // Ekle
-        await FirebaseFirestore.instance.collection('todos').add(todoData);
-      }
-      Get.back();
-    }
-  }
+  TodoFormPage({this.todoId, this.isUpdate = false});
 
   @override
   Widget build(BuildContext context) {
+    final TodoController todoController = Get.find();
+    final TextEditingController titleController = TextEditingController();
+
+    if (isUpdate && todoId != null) {
+      // Todo'yu güncelleme durumunda, ID'ye göre verileri yükleyin
+      todoController.getTodoById(todoId!).then((todo) {
+        titleController.text = todo['title'] ?? '';
+      }).catchError((error) {
+        // Hata işleme
+        print("Error fetching todo: $error");
+      });
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text('Todo Formu')),
+      appBar: AppBar(
+        title: Text(isUpdate ? 'Update Todo' : 'Add Todo'),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _titleController,
-                decoration: InputDecoration(labelText: 'Başlık'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Başlık girin';
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                final title = titleController.text.trim();
+                if (title.isNotEmpty) {
+                  if (isUpdate && todoId != null) {
+                    await todoController.updateTodo(todoId!, {'title': title});
+                  } else {
+                    await todoController.addTodo({
+                      'title': title,
+                      'completed': false,
+                      'createdAt': Timestamp.now(),
+                    });
                   }
-                  return null;
-                },
-              ),
-              CheckboxListTile(
-                title: Text('Tamamlandı'),
-                value: _isCompleted,
-                onChanged: (value) {
-                  setState(() {
-                    _isCompleted = value ?? false;
-                  });
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _saveTodo,
-                child: Text('Kaydet'),
-              ),
-            ],
-          ),
+                  Get.back(); // Geri dön
+                }
+              },
+              child: Text(isUpdate ? 'Update' : 'Add'),
+            ),
+          ],
         ),
       ),
     );
