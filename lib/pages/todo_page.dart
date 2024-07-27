310894c93c794d5975719b220dc0727e7a950d6c
Elif Özcan <ozcann159@gmail.com>
2024-07-27 20:49:28 +0300

    Add search functionality to TodoPage

--- lib/pages/todo_page.dart
+++ lib/pages/todo_page.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/todo_controller.dart';
 import 'completed_todos_page.dart';
 import 'pending_todos_page.dart';
+import 'todo_form_page.dart';
 
 class TodoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final TodoController todoController = Get.put(TodoController());
+
     return DefaultTabController(
-      length: 2, // İki tab
+      length: 2,
       child: Scaffold(
         appBar: AppBar(
           title: Text('Todo List'),
-          bottom: TabBar(
-            tabs: [
-              Tab(text: 'Tamamlanmış'),
-              Tab(text: 'Tamamlanmamış'),
-            ],
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(100.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Ara',
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      todoController.searchQuery.value = value;
+                    },
+                  ),
+                ),
+                TabBar(
+                  tabs: [
+                    Tab(text: 'Tamamlanmış'),
+                    Tab(text: 'Tamamlanmamış'),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
         body: TabBarView(
@@ -22,6 +47,13 @@ class TodoPage extends StatelessWidget {
             CompletedTodosPage(),
             PendingTodosPage(),
           ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Get.to(() => TodoFormPage());
+          },
+          child: Icon(Icons.add),
+          tooltip: 'Todo Ekle',
         ),
       ),
     );
