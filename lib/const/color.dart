45dd52036c99e9db9c8e98e59d4ddbb7cae99887
Elif Özcan <ozcann159@gmail.com>
2024-07-27 21:07:57 +0300

    Add tdBlack and tdGrey colors to color constants

--- lib/const/color.dart
+++ lib/const/color.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 
 Color custom_green =  Color(0xff18DAA3);
 Color backgroundColors = Colors.white;
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF717171);
+
