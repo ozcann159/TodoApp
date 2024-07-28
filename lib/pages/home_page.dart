aa86359a869e741617a45aeaf181f640d8559a83
Elif Özcan <ozcann159@gmail.com>
2024-07-28 14:24:00 +0300

    Update HomePage to include logout functionality and navigation to TodoFormPage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,26 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:todo_task_app/pages/todo_page.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TodoPage();
-    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Get.offAllNamed('/login');
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Welcome to the Todo App!'),
+      ),
+    );
   }
 }
