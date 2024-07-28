461bd498e83468b0adf04bb1072c667f4543d9f4
Elif Özcan <ozcann159@gmail.com>
2024-07-28 14:24:59 +0300

    Update TodoListPage to include search functionality

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -14,7 +14,7 @@ class TodoListPage extends StatelessWidget {
         actions: [
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () => Get.to(() => TodoFormPage()),
+            onPressed: () => Get.to(() => TodoFormPage(todoId: null, isUpdate: false)),
           ),
         ],
       ),
