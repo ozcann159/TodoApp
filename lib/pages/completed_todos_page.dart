54bfa40abb89e4f2736e22f4bf3c2b4638b7c6b0
Elif Özcan <ozcann159@gmail.com>
2024-07-28 14:23:51 +0300

    Update CompletedTodosPage to display user-specific completed todos

--- lib/pages/completed_todos_page.dart
+++ lib/pages/completed_todos_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo_task_app/pages/todo_form_page.dart';
+
 import '../controllers/todo_controller.dart';
 
 class CompletedTodosPage extends StatelessWidget {
@@ -8,17 +10,45 @@ class CompletedTodosPage extends StatelessWidget {
     final TodoController todoController = Get.find();
 
     return Obx(() {
+      final todos = todoController.filteredCompletedTodos;
+      if (todos.isEmpty) {
+        return Center(child: Text('No completed todos'));
+      }
       return ListView.builder(
-        itemCount: todoController.filteredCompletedTodos.length,
+        itemCount: todos.length,
         itemBuilder: (context, index) {
-          var todo = todoController.filteredCompletedTodos[index];
-          return ListTile(
-            title: Text(todo['content']),
-            subtitle: Text(todo['createdAt'].toDate().toString()),
+          var todo = todos[index];
+
+          return Card(
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: ListTile(
+              leading: Icon(Icons.check_circle, color: Colors.green),
+              title: Text(
+                todo['title'] ?? 'No Title',
+                style: TextStyle(
+                  decoration: TextDecoration.lineThrough,
+                ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(todo['description'] ?? 'No Description'),
+                  Text(
+                    todo['createdAt']?.toDate().toString() ?? 'No Date',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                ],
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {
+                  Get.to(() => TodoFormPage(todoId: todo.id, isUpdate: true));
+                },
+              ),
+            ),
           );
         },
       );
     });
   }
 }
-
