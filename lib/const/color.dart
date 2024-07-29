bf006f6a6a7c65a989e2feda8bc39a042815d3bb
Elif Özcan <ozcann159@gmail.com>
2024-07-29 22:16:25 +0300

    color:Color palette updated

--- lib/const/color.dart
+++ lib/const/color.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-Color custom_green =  Color(0xff18DAA3);
-Color backgroundColors = Colors.white;
+Color custom_green = Color(0xff18DAA3);
+Color backgroundColors = Color(0xFF1d2630);
 
 const Color tdBlack = Color(0xFF3A3A3A);
 const Color tdGrey = Color(0xFF717171);
-
