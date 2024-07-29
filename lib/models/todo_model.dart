06b5fb5b55f09fa6e74658c5e06359da0865760d
Elif Özcan <ozcann159@gmail.com>
2024-07-29 11:56:29 +0300

    fix: Update Todo model for Timestamp handling and JSON serialization

---
+++ lib/models/todo_model.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:json_annotation/json_annotation.dart';
+import '../utils/timestamp_converter.dart';
+
+part 'todo_model.g.dart';
+
+@JsonSerializable()
+class Todo {
+  final String id;
+  final String title;
+  final String description;
+
+  @JsonKey(fromJson: _fromJsonTimestamp, toJson: _toJsonTimestamp)
+  final Timestamp createdAt;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.createdAt,
+  });
+
+  factory Todo.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Todo(
+      id: doc.id,
+      title: data['title'] ?? '',
+      description: data['description'] ?? '',
+      createdAt: TimestampConverter().fromJson(data['createdAt']),
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'title': title,
+      'description': description,
+      'createdAt': TimestampConverter().toJson(createdAt),
+    };
+  }
+
+  factory Todo.fromJson(Map<String, dynamic> json) => _$TodoFromJson(json);
+  Map<String, dynamic> toJson() => _$TodoToJson(this);
+
+  static Timestamp _fromJsonTimestamp(Map<String, dynamic> json) {
+    return TimestampConverter().fromJson(json);
+  }
+
+  static Map<String, dynamic> _toJsonTimestamp(Timestamp timestamp) {
+    return TimestampConverter().toJson(timestamp);
+  }
+}
