f0f9d61d5db7bf1af9c26bd6b803e960f351072f
Elif Özcan <ozcann159@gmail.com>
2024-07-29 11:59:07 +0300

    feat: Implement TodoService for Firestore CRUD operations

--- lib/services/todo_service.dart
+++ lib/services/todo_service.dart
@@ -1,34 +1,49 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_task_app/models/todo_model.dart';
 
 class TodoService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  // Tüm todo'ları getirir
-  Stream<List<DocumentSnapshot>> getTodos({required bool completed}) {
-    return _db
-        .collection('todos')
-        .where('completed', isEqualTo: completed)
-        .snapshots()
-        .map((snapshot) => snapshot.docs);
+  Stream<List<Todo>> getTodos() {
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      return Stream.value([]); // Kullanıcı giriş yapmamışsa boş bir liste döndür
+    }
+    return _db.collection('todos').doc(userId).collection('userTodos').snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) => Todo.fromFirestore(doc)).toList();
+    });
   }
 
   // Belirli bir todo'yu ID ile getirir
-  Future<DocumentSnapshot> getTodoById(String id) async {
-    return await _db.collection('todos').doc(id).get();
+  Future<Todo?> getTodoById(String id) async {
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) return null; // Kullanıcı giriş yapmamışsa null döndür
+    final doc = await _db.collection('todos').doc(userId).collection('userTodos').doc(id).get();
+    return doc.exists ? Todo.fromFirestore(doc) : null;
   }
 
   // Yeni bir todo ekler
-  Future<void> addTodo(Map<String, dynamic> todo) async {
-    await _db.collection('todos').add(todo);
+  Future<void> addTodo(Todo todo) async {
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId != null) {
+      await _db.collection('todos').doc(userId).collection('userTodos').add(todo.toFirestore());
+    }
   }
 
   // Var olan bir todo'yu günceller
-  Future<void> updateTodo(String id, Map<String, dynamic> data) async {
-    await _db.collection('todos').doc(id).update(data);
+  Future<void> updateTodo(String id, Todo todo) async {
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId != null) {
+      await _db.collection('todos').doc(userId).collection('userTodos').doc(id).update(todo.toFirestore());
+    }
   }
 
   // Var olan bir todo'yu siler
   Future<void> deleteTodo(String id) async {
-    await _db.collection('todos').doc(id).delete();
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId != null) {
+      await _db.collection('todos').doc(userId).collection('userTodos').doc(id).delete();
+    }
   }
 }
